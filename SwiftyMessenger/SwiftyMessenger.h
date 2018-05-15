fd5b28a933ae29a71d19932ef95b8dc441933db8
Abdullah Selek <abdullahselek@gmail.com>
2018-05-15 19:04:16 +0100

    Create empty project.

---
+++ SwiftyMessenger/SwiftyMessenger.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyMessenger.h
+//  SwiftyMessenger
+//
+//  Created by Abdullah Selek on 15.05.18.
+//  Copyright © 2018 Abdullah Selek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyMessenger.
+FOUNDATION_EXPORT double SwiftyMessengerVersionNumber;
+
+//! Project version string for SwiftyMessenger.
+FOUNDATION_EXPORT const unsigned char SwiftyMessengerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyMessenger/PublicHeader.h>
+
+
