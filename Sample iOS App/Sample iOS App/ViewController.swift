89e7822208a6b925855fa425b7532a57a1a80ec1
Abdullah Selek <abdullahselek@gmail.com>
2018-06-04 21:37:29 +0100

    Implement tableview cells and selection.

--- Sample iOS App/Sample iOS App/ViewController.swift
+++ Sample iOS App/Sample iOS App/ViewController.swift
@@ -11,6 +11,8 @@ import SwiftyMessenger
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+
     private static let groupIdentifier = "group.com.abdullahselek.swiftymessenger"
     private static let directory = "messenger"
 
@@ -20,6 +22,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        tableView.reloadData()
 
         messenger = Messenger(withApplicationGroupIdentifier: ViewController.groupIdentifier, directory: ViewController.directory)
         messengerListeningSession = MessengerSession.shared
@@ -34,6 +38,21 @@ class ViewController: UIViewController {
         watchConnectivityMessenger.listenForMessage(withIdentifier: "button") { message in
 
         }
+        messengerListeningSession.activateSession()
+    }
+
+}
+
+extension ViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let tableViewCell = tableView.dequeueReusableCell(withIdentifier: "TableViewCellIdentifier") as! TextTableViewCell
+        tableViewCell.titleLabel?.text = String(format: "%d", indexPath.row)
+        return tableViewCell
     }
 
 }
@@ -41,7 +60,18 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDelegate {
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let tableViewCell = tableView.cellForRow(at: indexPath) as? TextTableViewCell
+        let title = tableViewCell?.titleLabel.text
 
+        // Pass a message for the selection identifier.
+        messenger.passMessage(message: ["selectedCell": title], identifier: "selection")
+        watchConnectivityMessenger.passMessage(message: ["selectedCell": title], identifier: "selection")
     }
+
+}
+
+class TextTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
 
 }
