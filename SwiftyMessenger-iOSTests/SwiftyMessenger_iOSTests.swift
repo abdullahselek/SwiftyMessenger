0576c1d25779383ad400d5d85c6756078231b161
Abdullah Selek <abdullahselek@gmail.com>
2018-06-07 11:48:25 +0100

    Restructuring framework schemes.

--- SwiftyMessenger-iOSTests/SwiftyMessenger_iOSTests.swift
+++ SwiftyMessenger-iOSTests/SwiftyMessenger_iOSTests.swift
@@ -11,38 +11,39 @@ import XCTest
 @testable import SwiftyMessenger
 
 class MessengerFileTransitingTests: XCTestCase {
-
+    
     var fileTransiting: MessengerFileTransiting!
-
+    
     override func setUp() {
         super.setUp()
         fileTransiting = MessengerFileTransiting(withApplicationGroupIdentifier: "com.abdullahselek.SwiftyGroup", directory: "tests")
     }
-
+    
     func testInit() {
         XCTAssertNotNil(MessengerFileTransiting(), "MessengerFileTransiting default initialization failed!")
     }
-
+    
     func testInitWithParameter() {
         XCTAssertNotNil(fileTransiting, "MessengerFileTransiting initialization with parameters failed!")
     }
-
+    
     func testMessagePassingDirectoryPath() {
         XCTAssertNil(fileTransiting.messagePassingDirectoryPath())
     }
-
+    
     func testFilePath_whenIdentifierEmpty() {
         XCTAssertNil(fileTransiting.filePath(forIdentifier: ""))
     }
-
+    
     func testFilePath_whenIdentifierNotEmpty() {
         // because of testing framework there is no app group available
         XCTAssertNil(fileTransiting.filePath(forIdentifier: "file1"))
     }
-
+    
     override func tearDown() {
         fileTransiting = nil
         super.tearDown()
     }
-
+    
 }
+
