b476883ba59b7c11def1a767d45fa212961402b1
Abdullah Selek <abdullahselek@gmail.com>
2018-07-16 20:16:12 +0100

    Create today extension in sample app.

---
+++ Sample iOS App/Sample Today Extension/TodayViewController.swift
@@ -0,0 +1,34 @@
+//
+//  TodayViewController.swift
+//  Sample Today Extension
+//
+//  Created by Abdullah Selek on 16.07.18.
+//  Copyright © 2018 Abdullah Selek. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view from its nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+        
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        
+        completionHandler(NCUpdateResult.newData)
+    }
+    
+}
